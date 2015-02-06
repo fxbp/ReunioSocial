dd6b7892dfdb580a93d9f46bfbd6480b85ed3763
elizabeth <omy2b@hotmail.com>
2015-02-06 20:37:00 +0100

    totes les clases

---
+++ ReunioSocial/ReunioSocial/Convidat.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReunioSocial
+{
+    public abstract class Convidat : Persona
+    {
+        protected int[] simpaties;
+        protected int plusSexe;
+        /// <summary>
+        /// Crea un convidat
+        /// </summary>
+        /// <param name="nom">string que l'identificarà</param>
+        /// <param name="simp">Taula de simpaties</param>
+        /// <param name="sex">Plus de simpatia sobre el sexe contrari</param>
+        public Convidat(string nom, int[] simp, int sexe)
+        { }
+
+
+        public Convidat();
+
+        /// <summary>
+        /// Crea un convidat
+        /// </summary>
+        /// <param name="nom">Caràcter que l'identificarà</param>
+        /// <param name="sex">Plus de simpatia sobre el sexe contrari</param>
+        public Convidat(string nom, int sexe)
+        {
+            this.nom = nom;
+            this.plusSexe = sexe; 
+        }
+
+
+        /// <summary>
+        /// Retorna o estableix la simpaties envers a algú
+        /// </summary>
+        public int this[string nom]
+        {
+            get { return 0; }
+            set { this.nom = nom; }
+        }
+
+        /// <summary>
+        /// Retorna o estableix el plus de simpatia envers del sexe contrari
+        /// </summary>
+        public int PlusSexe
+        {
+            get { return 0; }
+            set { }
+        }
+
+        /// <summary>
+        /// Retorna que si és un convidat
+        /// </summary>
+        /// <returns>Cert</returns>
+        public override bool EsConvidat()
+        {
+            return true;
+        }
+    }
+}
---
+++ ReunioSocial/ReunioSocial/Dona.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReunioSocial
+{
+    public class Dona : Convidat
+    {
+
+
+        /// <summary>
+        /// Crea una Dona
+        /// </summary>
+        /// <param name="nom">String que identifica aquesta Dona</param>
+        /// <param name="simpa">Taula de simpaties envers els altres convidats</param>
+        /// <param name="sexe">Plus de simpatia envers convidats del sexe contrari</param>
+        public Dona(string nom, int[] simpa, int sexe)
+            : base(nom, simpa, sexe)
+        {
+            this.nom = nom;
+            this.simpaties = simpa;
+            this.plusSexe = sexe;
+        }
+
+
+        /// <summary>
+        /// Crea una Dona
+        /// </summary>
+        /// <param name="nom"> String que identifica aquesta Dona</param>
+        /// <param name="sexe">Plus de simpatia envers convidats del sexe contrari</param>
+        public Dona(string nom, int sexe)
+        {
+            this.nom = nom;
+            this.plusSexe = sexe;
+        }
+
+
+        /// <summary>
+        /// Interès d'aquest home per una posició
+        /// </summary>
+        /// <param name="pos">Posició per la qual s'interessa</param>
+        /// <returns>Interès quantificat</returns>
+        public override int Interes(Posicio pos)
+        {
+            return 0;
+        }
+    }
+}
